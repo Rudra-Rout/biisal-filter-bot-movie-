a22cb04cbd2f521cfb077f8bce10e90de0ba50d7
umar2111k <138141263+umar2111k@users.noreply.github.com>
2024-08-19 11:47:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ USERNAME = environ.get('USERNAME', "https://telegram.me/Spark_Developer")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002246184289'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/Moviez_Gang')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002172731336').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://pohewe9931:onlyfortry@try.bbc46.mongodb.net/?retryWrites=true&w=majority&appName=Try")
 
 DATABASE_NAME = environ.get('DATABASE_NAME', "TELEGRAM_BOT_INFO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
